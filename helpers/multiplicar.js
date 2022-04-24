4c401da9745b50385a338f87816d3a4bcf893da4
Eduardo Loo <eduardo.loo@pucp.pe>
2022-04-23 22:32:46 -0500

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const colors = require('colors');
+
+const crearArchivo = async( base = 5, listar = true, hasta = 10 ) => {
+
+    try {
+        const nombre = `tabla-${ base }.txt`;
+        let salida = '';
+        let consola = '';
+    
+        for (let i = 1; i <= hasta; i++) {
+            salida += `${ base } x ${ i } = ${ base * i }\n`;
+            consola += `${ base } ${ 'x'.green } ${ i } ${ '='.green } ${ base * i }\n`;
+        }
+
+        if ( listar ) {
+            console.log('========================================================'.green);
+            console.log('Tabla de:'.green, colors.blue(base));
+            console.log('========================================================'.green);
+            console.log(consola);            
+        }
+        fs.writeFileSync(`./salida/${ nombre }`, salida);
+        return nombre;
+    } catch (err) {
+        throw err;
+    }
+
+}
+module.exports = {
+    crearArchivo
+}
